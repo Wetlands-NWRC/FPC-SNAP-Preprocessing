24463cb6e9a9572035bc79933b7ac74f49873204
Hamilton97 <ryangilberthamilton@gmail.com>
2022-03-23 10:49:43 -0400

    Update install-snap.sh

--- SNAP-CMD-Line/snap/install-snap.sh
+++ SNAP-CMD-Line/snap/install-snap.sh
@@ -8,16 +8,30 @@ SNAPVER=8
 # avoid NullPointer crash during S-1 processing
 java_max_mem=10G
 
+source "/home/hammy97/miniconda3/etc/profile.d/conda.sh"
+
 # activte the conda snappy env
 source conda activate snappy
 
+root_dir="/src/snap/"
+
+SNAPPY_PATH="/home/hammy97/miniconda3/envs/snappy/bin/python"
+
 # install module 'jpy' (A bi-directional Python-Java bridge)
-git clone https://github.com/bcdev/jpy.git /src/snap/jpy
-pip install --upgrade pip wheel
-(cd /src/snap/jpy && python setup.py bdist_wheel)
-# hack because ./snappy-conf will create this dir but also needs *.whl files...
-mkdir -p /root/.snap/snap-python/snappy
-cp /src/snap/jpy/dist/*.whl "/root/.snap/snap-python/snappy"
+if [ ! -d "${root_dir}/jpy"] 
+then
+  git clone https://github.com/bcdev/jpy.git /src/snap/jpy
+  pip install --upgrade pip wheel
+  (cd /src/snap/jpy && python setup.py bdist_wheel)
+  # hack because ./snappy-conf will create this dir but also needs *.whl files...
+  mkdir -p /root/.snap/snap-python/snappy
+  cp /src/snap/jpy/dist/*.whl "/root/.snap/snap-python/snappy"
+fi
+
+if [ ! -f "/src/snap/response.varfile"]
+then
+  cp ./response.varfile $root_dir
+fi
 
 # install and update snap
 wget -q -O /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh \
